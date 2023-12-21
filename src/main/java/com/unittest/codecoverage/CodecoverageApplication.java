6310dddf3551ad44645edbff29f9ecdbbc31a1be
yasamingol <yasamingolzar.ce@gmail.com>
2023-12-21 01:18:53 -0800

    Test pair programming commit.
    Co-authored-by: YasminKad <yasmin.kadkhodaei@gmail.com >

--- src/main/java/com/unittest/codecoverage/CodecoverageApplication.java
+++ src/main/java/com/unittest/codecoverage/CodecoverageApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodecoverageApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CodecoverageApplication.class, args);
+
 	}
 
 }
