b40603933a2433e4708183ab54cfb855d5a5f2f4
yasamingol <yasamingolzar.ce@gmail.com>
2023-12-21 02:32:53 -0800

    Add tests for Person. Co-authored-by: YasminKad <yasmin.kadkhodaei@gmail.com>

--- src/test/java/com/unittest/codecoverage/validators/PersonTest.java
+++ src/test/java/com/unittest/codecoverage/validators/PersonTest.java
@@ -7,6 +7,7 @@ import static org.assertj.core.api.Assertions.*;
 
 public class PersonTest {
 
+
     @Test
     public void testGetName_shouldReturnDefaultValue() {
         Person person = new Person();
