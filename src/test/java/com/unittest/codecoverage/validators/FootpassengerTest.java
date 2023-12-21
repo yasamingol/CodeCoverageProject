15c2b0e65524d5516fa820e6ebaab337fa934b23
yasamingol <yasamingolzar.ce@gmail.com>
2023-12-21 02:14:04 -0800

    Add tests for Footpassenger. Co-authored-by: YasminKad <yasmin.kadkhodaei@gmail.com >

---
+++ src/test/java/com/unittest/codecoverage/validators/FootpassengerTest.java
@@ -0,0 +1,45 @@
+package com.unittest.codecoverage.validators;
+
+import com.unittest.codecoverage.models.Footpassenger;
+import com.unittest.codecoverage.models.TrafficLigth;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class FootpassengerTest {
+
+    @Test
+    public void testCrossedTheCrosswalk_shouldReturnDefaultValue() {
+        Footpassenger footpassenger = new Footpassenger();
+
+        boolean result = footpassenger.crossedTheCrosswalk();
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void testSetCrossedTheCrosswalk_shouldSetCorrectValue() {
+        Footpassenger footpassenger = new Footpassenger();
+
+        footpassenger.setCrossedTheCrosswalk(true);
+
+        assertThat(footpassenger.crossedTheCrosswalk()).isTrue();
+    }
+
+    @Test
+    public void testGetCrossedTrafficLigth_shouldReturnNullByDefault() {
+        Footpassenger footpassenger = new Footpassenger();
+
+        TrafficLigth result = footpassenger.getCrossedTrafficLigth();
+
+        assertThat(result).isNull();
+    }
+
+    @Test
+    public void testLookedToTheRight_shouldReturnDefaultValue() {
+        Footpassenger footpassenger = new Footpassenger();
+
+        boolean result = footpassenger.lookedToTheRight();
+
+        assertThat(result).isFalse();
+    }
+}
